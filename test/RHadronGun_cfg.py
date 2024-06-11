ce2e30a80b55bc26795f0142a8333dc80cd1b07f
ctdax <colbythomps@gmail.com>
2024-06-11 09:44:10 -0500

    Updated RHadronGun_cfg.py to be more of an RHadronGun

--- test/RHadronGun_cfg.py
+++ test/RHadronGun_cfg.py
@@ -4,9 +4,11 @@
 # Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
 # with command line options: Configuration/GenProduction/python/EXO-RunIISummer20UL18GENSIM-00010-fragment.py --python_filename EXO-RunIISummer20UL18GENSIM-00010_1_cfg.py --eventcontent RAWSIM --customise SimG4Core/CustomPhysics/Exotica_HSCP_SIM_cfi.customise,Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM --fileout file:EXO-RunIISummer20UL18GENSIM-00010.root --conditions 106X_upgrade2018_realistic_v4 --beamspot Realistic25ns13TeVEarly2018Collision --step GEN,SIM --geometry DB:Extended --era Run2_2018 --no_exec --mc
 import FWCore.ParameterSet.Config as cms
-
+from FWCore.ParameterSet.VarParsing import VarParsing
 from Configuration.Eras.Era_Run2_2018_cff import Run2_2018
 
+options = VarParsing('python')
+options.parseArguments()
 process = cms.Process('SIM',Run2_2018)
 
 # import of standard configurations
@@ -56,7 +58,7 @@ process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
         filterName = cms.untracked.string('')
     ),
     eventAutoFlushCompressedSize = cms.untracked.int32(20971520),
-    fileName = cms.untracked.string('file:EXO-RunIISummer20UL18GENSIM-00010.root'),
+    fileName = cms.untracked.string(options.outputFile),
     outputCommands = process.RAWSIMEventContent.outputCommands,
     splitLevel = cms.untracked.int32(0)
 )
@@ -69,78 +71,15 @@ process.genstepfilter.triggerConditions=cms.vstring("generation_step")
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, '106X_upgrade2018_realistic_v4', '')
 
-process.dirhadrongenfilter = cms.EDFilter("MCParticlePairFilter",
-    MaxEta = cms.untracked.vdouble(100.0, 100.0),
-    MinEta = cms.untracked.vdouble(-100, -100),
-    MinP = cms.untracked.vdouble(0.0, 0.0),
-    MinPt = cms.untracked.vdouble(0.0, 0.0),
-    ParticleCharge = cms.untracked.int32(0),
-    ParticleID1 = cms.untracked.vint32(
-        1000993, 1009213, 1009313, 1009323, 1009113, 
-        1009223, 1009333, 1091114, 1092114, 1092214, 
-        1092224, 1093114, 1093214, 1093224, 1093314, 
-        1093324, 1093334
-    ),
-    ParticleID2 = cms.untracked.vint32(
-        1000993, 1009213, 1009313, 1009323, 1009113, 
-        1009223, 1009333, 1091114, 1092114, 1092214, 
-        1092224, 1093114, 1093214, 1093224, 1093314, 
-        1093324, 1093334
-    ),
-    Status = cms.untracked.vint32(1, 1)
-)
 
-
-process.generator = cms.EDFilter("Pythia8ConcurrentGeneratorFilter",
-    PythiaParameters = cms.PSet(
-        parameterSets = cms.vstring(
-            'pythia8CommonSettings', 
-            'pythia8CP5Settings', 
-            'processParameters'
-        ),
-        processParameters = cms.vstring(
-            'SUSY:all = off', 
-            'SUSY:gg2gluinogluino = on', 
-            'SUSY:qqbar2gluinogluino = on', 
-            'RHadrons:allow  = on', 
-            'RHadrons:allowDecay = off', 
-            'RHadrons:setMasses = on', 
-            'RHadrons:probGluinoball = 0.1'
-        ),
-        pythia8CP5Settings = cms.vstring(
-            'Tune:pp 14', 
-            'Tune:ee 7', 
-            'MultipartonInteractions:ecmPow=0.03344', 
-            'MultipartonInteractions:bProfile=2', 
-            'MultipartonInteractions:pT0Ref=1.41', 
-            'MultipartonInteractions:coreRadius=0.7634', 
-            'MultipartonInteractions:coreFraction=0.63', 
-            'ColourReconnection:range=5.176', 
-            'SigmaTotal:zeroAXB=off', 
-            'SpaceShower:alphaSorder=2', 
-            'SpaceShower:alphaSvalue=0.118', 
-            'SigmaProcess:alphaSvalue=0.118', 
-            'SigmaProcess:alphaSorder=2', 
-            'MultipartonInteractions:alphaSvalue=0.118', 
-            'MultipartonInteractions:alphaSorder=2', 
-            'TimeShower:alphaSorder=2', 
-            'TimeShower:alphaSvalue=0.118', 
-            'SigmaTotal:mode = 0', 
-            'SigmaTotal:sigmaEl = 21.89', 
-            'SigmaTotal:sigmaTot = 100.309', 
-            'PDF:pSet=LHAPDF6:NNPDF31_nnlo_as_0118'
-        ),
-        pythia8CommonSettings = cms.vstring(
-            'Tune:preferLHAPDF = 2', 
-            'Main:timesAllowErrors = 10000', 
-            'Check:epTolErr = 0.01', 
-            'Beams:setProductionScalesFromLHEF = off', 
-            'SLHA:keepSM = on', 
-            'SLHA:minMassSM = 1000.', 
-            'ParticleDecays:limitTau0 = on', 
-            'ParticleDecays:tau0Max = 10', 
-            'ParticleDecays:allowPhotonRadiation = on'
-        )
+process.generator = cms.EDFilter("FlatRandomEGunProducer",
+    PGunParameters = cms.PSet(
+		MaxEta = cms.vdouble(0.0, 0.0),
+		MinEta = cms.vdouble(0.0, 0.0),
+		MinP = cms.untracked.vdouble(0.0, 0.0),
+		MinPt = cms.untracked.vdouble(50.0, 50.0),
+		PartID = cms.vint32(1000993),
+		ParticleCharge = cms.untracked.int32(0),
     ),
     SLHAFileForPythia8 = cms.string('Configuration/Generator/data/HSCP_gluino_1800_SLHA.spc'),
     comEnergy = cms.double(13000.0),
@@ -150,7 +89,7 @@ process.generator = cms.EDFilter("Pythia8ConcurrentGeneratorFilter",
     maxEventsToPrint = cms.untracked.int32(0),
     particleFile = cms.untracked.string('Configuration/Generator/data/particles_gluino_1800_GeV.txt'),
     pdtFile = cms.FileInPath('Configuration/Generator/data/hscppythiapdtgluino1800.tbl'),
-    processFile = cms.untracked.string('SimG4Core/CustomPhysics/data/RhadronProcessList.txt'),
+    processFile = cms.untracked.string(options.inputFiles[0]),
     pythiaHepMCVerbosity = cms.untracked.bool(False),
     pythiaPylistVerbosity = cms.untracked.int32(0),
     slhaFile = cms.untracked.string('Configuration/Generator/data/HSCP_gluino_1800_SLHA.spc'),
@@ -158,7 +97,7 @@ process.generator = cms.EDFilter("Pythia8ConcurrentGeneratorFilter",
 )
 
 
-process.ProductionFilterSequence = cms.Sequence(process.generator+process.dirhadrongenfilter)
+process.ProductionFilterSequence = cms.Sequence(process.generator)
 
 # Path and EndPath definitions
 process.generation_step = cms.Path(process.pgen)
@@ -166,34 +105,3 @@ process.simulation_step = cms.Path(process.psim)
 process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
 process.endjob_step = cms.EndPath(process.endOfProcess)
 process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
-
-# Schedule definition
-process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.endjob_step,process.RAWSIMoutput_step)
-from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
-associatePatAlgosToolsTask(process)
-# filter all path with the production filter sequence
-for path in process.paths:
-	getattr(process,path).insert(0, process.ProductionFilterSequence)
-
-# customisation of the process.
-
-# Automatic addition of the customisation function from SimG4Core.CustomPhysics.Exotica_HSCP_SIM_cfi
-from SimG4Core.CustomPhysics.Exotica_HSCP_SIM_cfi import customise 
-
-#call to customisation function customise imported from SimG4Core.CustomPhysics.Exotica_HSCP_SIM_cfi
-process = customise(process)
-
-# Automatic addition of the customisation function from Configuration.DataProcessing.Utils
-from Configuration.DataProcessing.Utils import addMonitoring 
-
-#call to customisation function addMonitoring imported from Configuration.DataProcessing.Utils
-process = addMonitoring(process)
-
-# End of customisation functions
-
-# Customisation from command line
-
-# Add early deletion of temporary data products to reduce peak memory need
-from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
-process = customiseEarlyDelete(process)
-# End adding early deletion
