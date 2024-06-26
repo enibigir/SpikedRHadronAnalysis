9bcfdda517c8ec68a20509aeab8d5a992fafd3dd
ctdax <colbythomps@gmail.com>
2024-06-26 18:14:01 +0200

    added RHadron dictionary to runRHadronGun.sh

--- plugins/runRHadronGun.sh
+++ plugins/runRHadronGun.sh
@@ -28,8 +28,34 @@ iterate_directories() {
 # Create a dictionary to store key-value pairs for R-Hadron name and PDGID
 declare -A d
 d["~g_rho+"]=1009213
-my_dict["key2"]="value2"
-my_dict["key3"]="value3"
+d["~g_K*0"]=1009313
+d["~g_K*+"]=1009323
+d["~g_rho0"]=1009113
+d["~g_omega"]=1009223
+d["~g_phi"]=1009333
+d["~g_Delta-"]=1091114
+d["~g_Delta0"]=1092114
+d["~g_Delta+"]=1092214
+d["~g_Delta++"]=1092224
+d["~g_Sigma*-"]=1093114
+d["~g_Sigma*0"]=1093214
+d["~g_Sigma*+"]=1093224
+d["~g_Xi*-"]=1093314
+d["~g_Xi*0"]=1093324
+d["~g_Omega-"]=1093334
+d["~g_rho-"]=-1009213
+d["~g_K*bar0"]=-1009313
+d["~g_K*-"]=-1009323
+d["~g_Deltabar+"]=-1091114
+d["~g_Deltabar0"]=-1092114
+d["~g_Deltabar-"]=-1092214
+d["~g_Deltabar--"]=-1092224
+d["~g_Sigma*bar+"]=-1093114
+d["~g_Sigma*bar0"]=-1093214
+d["~g_Sigma*bar-"]=-1093224
+d["~g_Xi*bar+"]=-1093314
+d["~g_Xi*bar0"]=-1093324
+d["~g_Omegabar+"]=-1093334
 
 # Check if the directory argument is provided
 if [ -z "$1" ]; then
